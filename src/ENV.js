963f0a11190a12cb5ce60f7f4177ebad1a52ee23
李大河呀 <52955253+lidaheyaya@users.noreply.github.com>
2023-02-13 17:50:10 +0800

    Update ENV.js

--- src/ENV.js
+++ src/ENV.js
@@ -4,4 +4,5 @@ module.exports = {
   AUTHORIZATION_CODE: process.env.AUTHORIZATION_CODE,
   DINGDING_WEBHOOK: process.env.DINGDING_WEBHOOK,
   PUSHPLUS_TOKEN: process.env.PUSHPLUS_TOKEN,
+  SEVER_TOKEN: process.env.SEVER_TOKEN,
 }
