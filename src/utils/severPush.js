217dd08182c95e72367006be3cc2482b87a4796f
李大河呀 <52955253+lidaheyaya@users.noreply.github.com>
2023-02-13 17:53:21 +0800

    Update severPush.js

--- src/utils/severPush.js
+++ src/utils/severPush.js
@@ -1,12 +1,13 @@
 const axios = require('axios')
+const { SEVER_TOKEN } = require('../ENV.js')
 const SUCCESS_CODE = 200
 
 const pushplus = async ({ title = '', content = '' } = {}) => {
   try {
     await axios
-      .post('https://sctapi.ftqq.com/SCT171405TOF67UqDftoBLwwCoid2k9e1k.send', data={
+      .post('https://sctapi.ftqq.com/'+ SEVER_TOKEN +'.send', data={
         text:title,
-        desc:content,
+        desp:content,
       })
       .then(response => {
         if (response?.data.code !== SUCCESS_CODE) {
