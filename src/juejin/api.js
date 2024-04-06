775c90a669721b520d46527af91e8bb880f069aa
dongwei1125 <2465204327@qq.com>
2024-04-06 21:59:27 +0800

    fix: 修复沾喜气下线导致的幸运值异常问题 #15

--- src/juejin/api.js
+++ src/juejin/api.js
@@ -49,38 +49,6 @@ class Api {
   }
 
   /**
-   * @desc 围观大奖记录
-   * @param page_no
-   * @param page_size
-   * @returns {Promise<*>}
-   * {
-   *   count: Number 数量
-   *   lotteries: [
-   *     {
-   *       history_id: String 记录 ID
-   *     }
-   *   ]
-   * }
-   */
-  getLotteryHistory({ page_no = 1, page_size = 5 } = {}) {
-    return this.http.post('/growth_api/v1/lottery_history/global_big', { page_no, page_size })
-  }
-
-  /**
-   * @desc 沾喜气
-   * @param lottery_history_id
-   * @returns {Promise<*>}
-   * {
-   *   dip_value: Number 幸运值
-   *   total_value: Number 总幸运值
-   *   has_dip: Boolean 是否沾过
-   * }
-   */
-  dipLucky(lottery_history_id) {
-    return this.http.post('/growth_api/v1/lottery_lucky/dip_lucky', { lottery_history_id })
-  }
-
-  /**
    * @desc 免费抽奖次数
    * @returns {Promise<*>}
    * {
@@ -109,6 +77,17 @@ class Api {
    */
   getCurrentPoint() {
     return this.http.get('/growth_api/v1/get_cur_point')
+  }
+
+  /**
+   * @desc 当前幸运值
+   * @returns {Promise<*>}
+   * {
+   *   total_value: 总幸运值
+   * }
+   */
+  getLucky() {
+    return this.http.post('/growth_api/v1/lottery_lucky/my_lucky')
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -10,8 +10,6 @@ const growth = {
   sumPoint: 0, // 总矿石数
   contCount: 0, // 连续签到天数
   sumCount: 0, // 累计签到天数
-  dippedLucky: false, // 是否沾喜气
-  dipValue: 0, // 幸运值
   luckyValue: 0, // 总幸运值
   freeCount: 0, // 免费抽奖次数
   freeDrawed: false, // 是否免费抽奖
@@ -27,7 +25,6 @@ ${growth.checkedIn ? `签到 +${growth.incrPoint} 矿石` : '今日已签到'}
 当前矿石数 ${growth.sumPoint}
 连续签到天数 ${growth.contCount}
 累计签到天数 ${growth.sumCount}
-${growth.dippedLucky ? '今日已经沾过喜气' : `沾喜气 +${growth.dipValue} 幸运值`}
 当前幸运值 ${growth.luckyValue}
 免费抽奖次数 ${growth.freeCount}
 ${growth.freeDrawed ? `恭喜抽中 ${growth.lotteryName}` : '今日已免费抽奖'}
@@ -62,19 +59,6 @@ const main = async () => {
   growth.contCount = counts.cont_count
   growth.sumCount = counts.sum_count
 
-  // 沾喜气
-  const lotteryHistory = await juejin.getLotteryHistory()
-  const lotteries = lotteryHistory.lotteries || []
-
-  if (lotteries.length > 0) {
-    const [firstLottery] = lotteries
-    const dipLuckyResult = await juejin.dipLucky(firstLottery.history_id)
-
-    growth.dippedLucky = dipLuckyResult.has_dip
-    growth.dipValue = dipLuckyResult.dip_value
-    growth.luckyValue = dipLuckyResult.total_value
-  }
-
   // 免费抽奖
   const lotteryConfig = await juejin.getLotteryConfig()
   growth.freeCount = lotteryConfig.free_count || 0
@@ -88,6 +72,10 @@ const main = async () => {
 
   // 当前矿石数
   growth.sumPoint = await juejin.getCurrentPoint()
+
+  // 当前幸运值
+  const luckyResult = await juejin.getLucky()
+  growth.luckyValue = luckyResult.total_value
 
   pushMessage({
     type: 'info',
