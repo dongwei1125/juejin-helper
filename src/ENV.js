1c8c54d45be53e7b44756f5c87787ce3a2d3e3e6
w157020087 <157020087@qq.com>
2024-08-07 09:49:32 +0800

    优化

--- src/ENV.js
+++ src/ENV.js
@@ -1,20 +1,20 @@
 /*
  * @Author: wangjiaxiang
  * @Date: 2024-08-06 17:40:40
- * @LastEditTime: 2024-08-07 09:32:34
+ * @LastEditTime: 2024-08-07 09:49:05
  * @LastEditors: wangjiaxiang
  * @Description: 
  * @FilePath: /juejin-helper/src/ENV.js
  * 
  */
 module.exports = {
-  COOKIE: "__tea_cookie_tokens_2608=%257B%2522user_unique_id%2522%253A%25227037344485533779463%2522%252C%2522web_id%2522%253A%25227037344485533779463%2522%252C%2522timestamp%2522%253A1641871239051%257D; store-region=cn-bj; store-region-src=uid; csrf_session_id=9778d9a10b200b33d6ed76148e4f2b6f; _tea_utm_cache_2608={%22utm_source%22:%22gold_browser_extension%22}; _tea_utm_cache_2018={%22utm_source%22:%22gold_browser_extension%22}; passport_csrf_token=76f7efc5aa5f05c1b42766d13ea6c94e; passport_csrf_token_default=76f7efc5aa5f05c1b42766d13ea6c94e; _tea_utm_cache_576092={%22utm_source%22:%22gold_browser_extension%22}; is_staff_user=false; odin_tt=d56735eb77719c6852d9da6178a172bb4d15c7750468ad8970adc51381620694f27219448388576c4f3ba24dc1335f67ca3df0e4e057f0707760a3ab37c6e7d9; n_mh=ITlicCLyq-Hy99elN5_rjN6j-d1rp8zd939ZDE8FG1M; passport_auth_status=1a74ca1957106b9d5b1222e5aee24675%2C; passport_auth_status_ss=1a74ca1957106b9d5b1222e5aee24675%2C; sid_guard=1fa1bb5cef76e2c3f95712d1b37a5dee%7C1722499953%7C31536000%7CFri%2C+01-Aug-2025+08%3A12%3A33+GMT; uid_tt=dd3c5cc90963c3f36252596d6f51e9ab; uid_tt_ss=dd3c5cc90963c3f36252596d6f51e9ab; sid_tt=1fa1bb5cef76e2c3f95712d1b37a5dee; sessionid=1fa1bb5cef76e2c3f95712d1b37a5dee; sessionid_ss=1fa1bb5cef76e2c3f95712d1b37a5dee; sid_ucp_v1=1.0.0-KDFjNzNkMDY1NzVjN2Q1ODUwNDJhMzA2YThjZTczNWUwZmNmYWZkMzUKFwin_pDl4fXjAxDxhq21BhiwFDgCQPEHGgJsZiIgMWZhMWJiNWNlZjc2ZTJjM2Y5NTcxMmQxYjM3YTVkZWU; ssid_ucp_v1=1.0.0-KDFjNzNkMDY1NzVjN2Q1ODUwNDJhMzA2YThjZTczNWUwZmNmYWZkMzUKFwin_pDl4fXjAxDxhq21BhiwFDgCQPEHGgJsZiIgMWZhMWJiNWNlZjc2ZTJjM2Y5NTcxMmQxYjM3YTVkZWU",
+  COOKIE: process.env.COOKIE,
   EMAIL: process.env.EMAIL,
   AUTHORIZATION_CODE: process.env.AUTHORIZATION_CODE,
   PUSHPLUS_TOKEN: process.env.PUSHPLUS_TOKEN,
   DINGDING_WEBHOOK: process.env.DINGDING_WEBHOOK,
   FEISHU_WEBHOOK: process.env.FEISHU_WEBHOOK,
-  MS_TOKEN:"50Phx8Oc91-sXy0yhABYoOYo-Eckw0NEyQiI2EajqUGZJrTwt-0dLgHBlEKuDcSLoAA6vXoZFHOVrWFczwmY6pa5vrO32WbdazXqcB2O0QmUWNtiILiDS3AIiVReH970",
-  A_BOGUS:"d7BYDc2eMsm1wE3BsXDz9JJmimf0YWRPgZEPavnv10LB",
-  UUID:"7037344485533779463",
+  MS_TOKEN:process.env.MS_TOKEN,
+  A_BOGUS:process.env.A_BOGUS,
+  UUID:process.env.UUID,
 }
